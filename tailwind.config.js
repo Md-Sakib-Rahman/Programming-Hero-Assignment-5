a8d8baabaaeb903135cc8c29371cfc20ea5532d2
Md-Sakib-Rahman <sakibrahman.user@gmail.com>
2025-08-28 15:08:24 +0600

    UI designed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+    // tailwind.config.js
+    module.exports = {
+      content: [
+        "./index.html", // Or adjust to your HTML file location
+        // Add other paths if you have HTML in subdirectories
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    };
